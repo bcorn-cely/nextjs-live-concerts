ca9aa1478a8ce7b2b59ca99adfb23a58629c350f
Brandon Corn <brandon.corn@vercel.com>
2024-09-17 21:46:26 -0600

    remove isr enable from layout, enabled in page

--- app/artist/[id]/layout.tsx
+++ app/artist/[id]/layout.tsx
@@ -1,9 +1,5 @@
 import { ReactNode } from "react";
 
-export async function generateStaticParams() {
-    return [];
-}
-
 export async function Layout({children}: { children: ReactNode }) {
     return children;
 }
